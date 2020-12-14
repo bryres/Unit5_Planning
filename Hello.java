52a9e63d6ee2cf096d3ad682a2616e0bcc342861
Bryan Respass <bryres@bergen.org>
2020-12-14 11:54:10 -0500

    shall to will

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("This is fun.");
-        System.out.println("What shall we do next?");
+        System.out.println("What will we do next?");
 
     }
 }
